9d7d9948213048de8c791810cfef0f7885a538d0
rakhmetzhanoff <dulat.rakhymetzhan@narxoz.kz>
2025-04-11 19:57:09 +0500

    auth+migration

--- cmd/main.go
+++ cmd/main.go
@@ -2,26 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"log"
-	"moviedb/internal/models"
+	"moviedb/internal/db"
 	"moviedb/internal/routes"
 )
 
 func main() {
-	db, err := gorm.Open(postgres.Open("postgres://movie_user:movie_password@localhost:5444/movie_database?sslmode=disable"), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Error connecting to the database:", err)
-	}
-
-	err = db.AutoMigrate(&models.Movie{})
-	if err != nil {
-		log.Fatal("Error on migrating to the DB", err)
-	}
+	db.InitDB()
 
 	r := gin.Default()
-	routes.SetupRoutes(r, db)
-
+	routes.SetupRoutes(r)
 	r.Run(":8080")
 }
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,25 +2,28 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
+	"moviedb/internal/auth"
+	"moviedb/internal/db"
 	"moviedb/internal/delivery"
 	"moviedb/internal/repository"
 	"moviedb/internal/services"
 )
 
-func SetupRoutes(r *gin.Engine, db *gorm.DB) {
-	movieRepo := repository.NewMovieRepository(db)
+func SetupRoutes(r *gin.Engine) {
+	r.POST("/login", auth.Login)
+	r.POST("/register", auth.Register)
 
-	movieService := services.NewMovieService(movieRepo)
-
-	movieHandler := delivery.NewMovieHandler(movieService)
+	dbInstance := db.DB
+	repo := repository.NewMovieRepository(dbInstance)
+	service := services.NewMovieService(repo)
+	handler := delivery.NewMovieHandler(service)
 
 	movies := r.Group("api/v1/movies")
 	{
-		movies.GET("/", movieHandler.GetAllMovies)
-		movies.POST("/", movieHandler.CreateMovie)
-		movies.GET("/:id", movieHandler.GetMovieByID)
-		movies.PUT("/:id", movieHandler.UpdateMovie)
-		movies.DELETE("/:id", movieHandler.DeleteMovie)
+		movies.GET("/", handler.GetAllMovies)
+		movies.GET("/:id", handler.GetMovieByID)
+		movies.POST("/", handler.CreateMovie)
+		movies.PUT("/:id", handler.UpdateMovie)
+		movies.DELETE("/:id", handler.DeleteMovie)
 	}
 }
